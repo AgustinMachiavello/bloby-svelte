7166d559f02661d5d1c3ca39fdf6e8f24ddc00e5
AgustinMachiavello <agmachiavello@gmail.com>
2022-07-11 23:51:57 +0200

    first attempt to component

---
+++ src/lib/Blob.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  const seed: string = "";
+  const animate = (): void => {
+    console.warn("animate");
+  };
+</script>
+
+<div id="blob">Hello world</div>
+
+<style>
+  #blob {
+    width: 200px;
+    height: 200px;
+    border-radius: 50% 50% 50% 50% / 30% 30% 30% 30%;
+    background-color: pink;
+    animation: stand 2s infinite;
+    animation-timing-function: ease-in-out;
+    animation-direction: alternate-reverse;
+  }
+
+  @keyframes stand {
+    from {
+      width: 200px;
+      height: 200px;
+    }
+    to {
+      width: 300px;
+      height: 100px;
+      border-radius: 30% 45% 67% 12% / 57% 89% 34% 22%;
+    }
+  }
+</style>
